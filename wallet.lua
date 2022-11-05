efd2e7db1093e6a864d4fc762d1c90aec54589c6
Manav Singh <MANVENDRAS2608@GMAIL.COM>
2022-11-05 09:28:21 +0530

    Update
    Test

--- wallet.lua
+++ wallet.lua
@@ -4,6 +4,7 @@ QBCore.Functions.CreateUseableItem('wallet' , function(source)
     local Player = QBCore.Functions.GetPlayer(source)
     local bankamount = Player.PlayerData.money.bank
     local cashamount = Player.PlayerData.money.cash
+    
     TriggerClientEvent('hud:client:ShowAccounts', source, 'cash', cashamount)
     TriggerClientEvent('hud:client:ShowAccounts', source, 'bank', bankamount)
 end)
